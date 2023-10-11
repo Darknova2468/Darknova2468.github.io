99cf1afb1aa5fe21273beb4fbd875fbd1a70c601
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-10-11 11:37:25 -0600

    refactoring and win detection

--- 02-Maze Solver/sketch.js
+++ 02-Maze Solver/sketch.js
@@ -14,6 +14,7 @@ function setup() {
     levels = {
       mazes: loadLevels(data),
       currentMaze: 0,
+      mazeNumbers: 2
     };
   });
   colorCode = [null, color(192), color(64), color(2, 204, 254), color(2, 204, 254), color(255, 36, 0)];
@@ -32,36 +33,29 @@ function drawMaze(_maze){
     for(let j=0; j<_maze.dimension[1]; j++){
       if(_maze.mazeMap[i][j] !== 0){
         fill(colorCode[_maze.mazeMap[i][j]]);
-        let x = _maze.cellSize[0]*(i+1.5+0.5*((j+1)%2));
-        let y = _maze.cellSize[1]*(j+2);
+        let [x, y] = worldToScreen([i,j], _maze.cellSize, [1.5, 2]);
         ellipse(x, y, _maze.cellSize[0], _maze.cellSize[1]);
       }
     }
   }
   //draws player;
   fill(0, 255, 0);
-  let playerPos = _maze.player.pos;
-  let x = _maze.cellSize[0]*(playerPos[0]+1.5+0.5*((playerPos[1]+1)%2));
-  let y = _maze.cellSize[1]*(playerPos[1]+2);
+  let [x, y] = worldToScreen(_maze.player.pos, _maze.cellSize, [1.5, 2]);
   ellipse(x, y, _maze.cellSize[1], _maze.cellSize[1]*0.5);
 
   //draw players next move;
   if(_maze.player.nextPos !== null){
-    let nextPlayerPos = _maze.player.nextPos;
-    let x = _maze.cellSize[0]*(nextPlayerPos[0]+1.5+0.5*((nextPlayerPos[1]+1)%2));
-    let y = _maze.cellSize[1]*(nextPlayerPos[1]+2);
+    let [x, y] = worldToScreen(_maze.player.nextPos, _maze.cellSize, [1.5, 2]);
     fill(0, 255, 0);
     ellipse(x, y, _maze.cellSize[1], _maze.cellSize[1]/2);
-    fill(colorCode[_maze.mazeMap[nextPlayerPos[0]][nextPlayerPos[1]]]);
+    fill(colorCode[_maze.mazeMap[_maze.player.nextPos[0]][_maze.player.nextPos[1]]]);
     ellipse(x, y, _maze.cellSize[1]/2, _maze.cellSize[1]/4);
   }
 
   //draws enemies;
   fill(255, 165, 0);
   for(let i=0; i<_maze.enemies.length; i++){
-    let enemyPos = _maze.enemies[i].pos;
-    let x = _maze.cellSize[0]*(enemyPos[0]+1.5+0.5*((enemyPos[1]+1)%2));
-    let y = _maze.cellSize[1]*(enemyPos[1]+2);
+    let [x, y] = worldToScreen(_maze.enemies[i].pos, _maze.cellSize, [1.5, 2]);
     ellipse(x, y, _maze.cellSize[1], _maze.cellSize[1]*0.5);
   }
 }
@@ -77,5 +71,17 @@ function mouseReleased(){
         currentMaze.player.pos, 
         currentMaze.dimension);
     }
+    //checks if you have beat the level
+    if(currentMaze.end[0] === currentMaze.player.pos[0] &&
+       currentMaze.end[1] === currentMaze.player.pos[1] &&
+      levels.currentMaze + 1 < levels.mazeNumbers){
+      levels.currentMaze++;
+    }
   }
+}
+
+function worldToScreen(_pos, _scale, _offset){
+  let x = _scale[0]*(_pos[0]+_offset[0]+0.5*((_pos[1]+1)%2));
+  let y = _scale[1]*(_pos[1]+_offset[1]);
+  return [x,y];
 }
