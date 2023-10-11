5571c24112a3d23e30d99740289e44a0a1ec33fa
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-10-11 09:26:50 -0600

    finished refactoring code **Remember to rewrite the castNext function in entities.js**

--- 02-Maze Solver/entities.js
+++ 02-Maze Solver/entities.js
@@ -1,11 +1,83 @@
 class Player{
   constructor(_pos){
     this.pos = _pos;
+    this.nextPos = null;
+  }
+  updatePos(){
+    this.pos = this.nextPos;
+  }
+  castNext(_maze){
+    let pointer;
+    let cases = [
+      [0, 1],
+      [4, 3]
+    ];
+    let case1 = [
+      [-1, -1],
+      [0, -1],
+      [1, 0],
+      [0, 1],
+      [-1, 1],
+      [-1, 0]
+    ];
+    let case2 = [
+      [0, -1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [-1,0]
+    ];
+    let playerX = _maze.cellSize[0]*(this.pos[0]+1.5+0.5*((this.pos[1]+1)%2));
+    let playerY = _maze.cellSize[1]*(this.pos[1]+2);
+    let magnitude = dist(playerX, playerY, mouseX, mouseY);
+    let theta = Math.atan((mouseX-playerX)/(mouseY-playerY));
+    if(magnitude <= 2*_maze.cellSize[0]){
+      if(Math.abs(theta)-1.309 > 0){
+        if(playerX > mouseX){
+          pointer = 5;
+        }
+        else {
+          pointer = 2;
+        }
+        
+      }
+      else {
+        pointer = cases[1*(playerY<mouseY)][1*(playerX<mouseX)];
+      }
+      let x; let y;
+      if(this.pos[1]%2 === 1){
+        x = this.pos[0]+case1[pointer][0];
+        y = this.pos[1]+case1[pointer][1];
+      }
+      else {
+        x = this.pos[0]+case2[pointer][0];
+        y = this.pos[1]+case2[pointer][1];
+      }
+      if(x< _maze.dimension[0] && x>=0 && y<_maze.dimension[1] && y>= 0){
+        if(_maze.mazeMap[x][y] !== 0 && _maze.mazeMap[x][y] !== 2){
+          this.nextPos = [x, y];
+          return [x, y];
+        }
+        this.nextPos = null;
+        return null;
+      }
+      this.nextPos = null;
+      return null;
+    }
+    this.nextPos = null;
+    return null;
   }
 }
 
 class Enemy{
   constructor(_pos){
     this.pos = _pos;
+    this.path;
+  }
+  updatePos(_graph, _player, _dimension){
+    this.path = _graph.solve(this.pos, _player);
+    let id = this.path.shift();
+    this.pos = [Math.floor(id/_dimension[1]),id%_dimension[1]];
   }
 }
--- 02-Maze Solver/sketch.js
+++ 02-Maze Solver/sketch.js
@@ -6,24 +6,23 @@
 // - describe what you did to take this project "above and beyond"
 
 let levels;
-let data;
 let colorCode; 
-
-function preload(){
-  data = loadStrings("levels.txt");
-}
 
 function setup() {
   createCanvas(600, 300);
-  levels = {
-    mazes: loadLevels(data),
-    currentMaze: 0,
-  };
+  loadStrings("levels.txt", function(data){
+    levels = {
+      mazes: loadLevels(data),
+      currentMaze: 0,
+    };
+  });
   colorCode = [null, color(192), color(64), color(2, 204, 254), color(2, 204, 254), color(255, 36, 0)];
 }
 
 function draw() {
   background(220);
+  levels.mazes[levels.currentMaze].player.castNext(levels.mazes[levels.currentMaze]);
+  //draws maze;
   drawMaze(levels.mazes[levels.currentMaze]);
 }
 
@@ -46,6 +45,17 @@ function drawMaze(_maze){
   let y = _maze.cellSize[1]*(playerPos[1]+2);
   ellipse(x, y, _maze.cellSize[1], _maze.cellSize[1]*0.5);
 
+  //draw players next move;
+  if(_maze.player.nextPos !== null){
+    let nextPlayerPos = _maze.player.nextPos;
+    let x = _maze.cellSize[0]*(nextPlayerPos[0]+1.5+0.5*((nextPlayerPos[1]+1)%2));
+    let y = _maze.cellSize[1]*(nextPlayerPos[1]+2);
+    fill(0, 255, 0);
+    ellipse(x, y, _maze.cellSize[1], _maze.cellSize[1]/2);
+    fill(colorCode[_maze.mazeMap[nextPlayerPos[0]][nextPlayerPos[1]]]);
+    ellipse(x, y, _maze.cellSize[1]/2, _maze.cellSize[1]/4);
+  }
+
   //draws enemies;
   fill(255, 165, 0);
   for(let i=0; i<_maze.enemies.length; i++){
@@ -56,70 +66,16 @@ function drawMaze(_maze){
   }
 }
 
-//incorperate this function into the player class under entitites.js
-/* 
-function castNext(_pos){
-  let pointer;
-  let cases = [
-    [0, 1],
-    [4, 3]
-  ];
-  let case1 = [
-    [-1, -1],
-    [0, -1],
-    [1, 0],
-    [0, 1],
-    [-1, 1],
-    [-1, 0]
-  ];
-  let case2 = [
-    [0, -1],
-    [1, -1],
-    [1, 0],
-    [1, 1],
-    [0, 1],
-    [-1,0]
-  ];
-  let playerX = maze.cellWidth*(_pos[0]+1.75+0.5*((_pos[1]+1)%2));
-  let playerY = _pos[1]*maze.cellHeight+0.25*height;
-  let magnitude = dist(playerX, playerY, mouseX, mouseY);
-  let theta = Math.atan((mouseX-playerX)/(mouseY-playerY));
-  if(magnitude <= 2*maze.cellWidth){
-    if(Math.abs(theta)-1.309 > 0){
-      if(playerX > mouseX){
-        pointer = 5;
-      }
-      else {
-        pointer = 2;
-      }
-      
+function mouseReleased(){
+  //moves player and enemies;
+  if(levels.mazes[levels.currentMaze].player.nextPos !== null){
+    let currentMaze = levels.mazes[levels.currentMaze];
+    currentMaze.player.updatePos();
+    for(let i=0; i<levels.mazes[levels.currentMaze].enemies.length; i++){
+      currentMaze.enemies[i].updatePos(
+        currentMaze.graph, 
+        currentMaze.player.pos, 
+        currentMaze.dimension);
     }
-    else {
-      pointer = cases[1*(playerY<mouseY)][1*(playerX<mouseX)];
-    }
-    let x; let y;
-    if(_pos[1]%2 === 1){
-      x = _pos[0]+case1[pointer][0];
-      y = _pos[1]+case1[pointer][1];
-    }
-    else {
-      x = _pos[0]+case2[pointer][0];
-      y = _pos[1]+case2[pointer][1];
-    }
-    if(x< maze.dimension[0] && x>=0 && y<maze.dimension[1] && y>= 0){
-      if(maze.mazeMap[x][y] !== 0 && maze.mazeMap[x][y] !== 2){
-        let screenX = maze.cellWidth*(x+1.75+0.5*((y+1)%2));
-        let screenY = y*maze.cellHeight+0.25*height;
-        fill(0, 200, 200);
-        ellipse(screenX, screenY, maze.cellWidth*0.5, maze.cellHeight*0.5);
-        fill(192);
-        ellipse(screenX, screenY, maze.cellWidth*0.25, maze.cellHeight*0.25);
-        return [x, y];
-      }
-      return null;
-    }
-    return null;
   }
-  return null;
 }
-*/
