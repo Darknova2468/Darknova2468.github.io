a043f05ee00cdba1e5f244583a93980ed95d0377
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-10-10 22:15:50 -0600

    made new file that loads all mazes in  the levels.txt in setup **Remember to implement call backs such that the preload function in sketch.js is uneeded

---
+++ 02-Maze Solver/mazeLoader.js
@@ -0,0 +1,44 @@
+class Maze {
+  constructor(_id, _mazeMap, _dimension, _player, _end, _enemies){
+    this.id = _id;
+    this.mazeMap = _mazeMap;
+    this.dimension = _dimension;
+    this.player = _player;
+    this.end = _end;
+    this.enemies = _enemies;
+    this.graph = new Graph(this.mazeMap, this.dimension);
+    if(width/2 > height){
+      this.cellSize = [width/10, width/20];
+    }
+    else {
+      this.cellSize = [height/5, height/10];
+    }
+  }
+}
+
+function loadLevels(_data){
+  let levels = [];
+  for(let i=0; i<_data.shift(); i++){
+    let dimension = _data.shift().split(",", 2).map(Number);
+    dimension = [parseInt(dimension[0]), parseInt(dimension[1])];
+    let mazeMap = new Array(dimension[0]);
+    for(let j=0; j<mazeMap.length; j++){
+      mazeMap[j] = new Array(dimension[1]).fill(null);
+    }
+    for(let j=0; j<dimension[1]; j++){
+      let row = _data.shift().split(",", dimension[0]).map(Number);
+      for(let k=0; k<dimension[0]; k++){
+        mazeMap[k][j] = row.shift();
+      }
+    }
+    let start = new Player(_data.shift().split(",", 2).map(Number));
+    let end = _data.shift().split(",", 2).map(Number);
+    let enemies = [];
+    let n =_data.shift();
+    for(let i=0; i<n; i++){
+      enemies.push(new Enemy(_data.shift().split(",", 2).map(Number)));
+    }
+    levels.push(new Maze(i, mazeMap, dimension, start, end, enemies));
+  }
+  return levels;
+}
