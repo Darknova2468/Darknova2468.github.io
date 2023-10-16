c245d598dbb04ce7266a647c358d0402c62ce1cd
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-10-16 15:10:33 -0600

    some code refactoring

--- 02-Maze Solver/entities.js
+++ 02-Maze Solver/entities.js
@@ -29,25 +29,9 @@ class Player{
       }
     }
     let pointer;
-    let cases = [
-      [0, 1],
-      [4, 3]
-    ];
-    let case1 = [
-      [-1, -1],
-      [0, -1],
-      [1, 0],
-      [0, 1],
-      [-1, 1],
-      [-1, 0]
-    ];
-    let case2 = [
-      [0, -1],
-      [1, -1],
-      [1, 0],
-      [1, 1],
-      [0, 1],
-      [-1,0]
+    let direction = [
+      [[0, -1],[1, -1],[1, 0],[0, 1],[1, 1],[-1,0]],
+      [[-1, -1],[0, -1],[1, 0],[-1, 1],[0, 1],[-1, 0]]
     ];
     let playerX = _maze.cellSize[0]*(this.pos[0]+_maze.offset[0]+0.5*((this.pos[1]+1)%2))+_maze.cellSize[1];
     let playerY = _maze.cellSize[1]*(this.pos[1]+_maze.offset[1])+_maze.cellSize[1]/1.25;
@@ -55,49 +39,30 @@ class Player{
     let theta = Math.atan((mouseX-playerX)/(mouseY-playerY));
     if(magnitude <= 2*_maze.cellSize[0]){
       if(Math.abs(theta)-1.309 > 0){
-        if(playerX > mouseX){
-          pointer = 5;
-        }
-        else {
-          pointer = 2;
-        }
-        
+        pointer = playerX>mouseX ? 5:2;
       }
       else {
-        pointer = cases[1*(playerY<mouseY)][1*(playerX<mouseX)];
+        pointer = (playerY<mouseY)*3+(playerX<mouseX);
       }
-      let x; let y;
-      if(this.pos[1]%2 === 1){
-        x = this.pos[0]+case1[pointer][0];
-        y = this.pos[1]+case1[pointer][1];
-      }
-      else {
-        x = this.pos[0]+case2[pointer][0];
-        y = this.pos[1]+case2[pointer][1];
-      }
+      let x = this.pos[0]+direction[this.pos[1]%2][pointer][0];
+      let y = this.pos[1]+direction[this.pos[1]%2][pointer][1];
       if(x< _maze.dimension[0] && x>=0 && y<_maze.dimension[1] && y>= 0){
         if(_maze.mazeMap[x][y] !== 0 && _maze.mazeMap[x][y] !== 2){
           for(let i=0;i<_maze.enemies.length; i++){
             if(_maze.enemies[i].pos[0] === x && 
-                   _maze.enemies[i].pos[1] === y){
+               _maze.enemies[i].pos[1] === y){
               if(this.powerUp === true){
                 this.nextPos = [x, y, true, i];
                 return [x, y, true, i];
               } 
-              else{
-                this.nextPos = null;
-                return null;
-              }
+              this.nextPos = null;
+              return null;
             }
           }
           this.nextPos = [x, y, false];
           return [x, y, false];
         }
-        this.nextPos = null;
-        return null;
       }
-      this.nextPos = null;
-      return null;
     }
     this.nextPos = null;
     return null;
